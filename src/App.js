21afb9c40e63b6ab4a902db6dd56253f8493740a
Ashwin1729 <mathematicswroteus@gmail.com>
2023-08-08 20:06:43 +0530

    Side Bar implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React, { useState, useRef } from "react";
+import { Layout, Menu, Breadcrumb, Row, Col, Switch as Switchs } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  AppstoreOutlined,
+  QuestionCircleOutlined,
+  InboxOutlined,
+  FolderOpenOutlined,
+  BarsOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import SideBar from "./components/SideBar";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  const [width, setWidth] = useState(0);
+
+  const layoutRef = useRef();
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
+
+  return (
+    <Layout style={{ minHeight: "100vh" }} ref={layoutRef}>
+      <SideBar />
+    </Layout>
+  );
 };
 
 export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import { Layout, Menu, Row } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  AppstoreOutlined,
+  QuestionCircleOutlined,
+  InboxOutlined,
+  FolderOpenOutlined,
+  BarsOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import styles from "./SideBar.module.css";
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [width, setWidth] = useState(0);
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
+
+  return (
+    <Sider
+      breakpoint="md"
+      collapsed={collapsed}
+      onCollapse={onCollapse}
+      className={styles.sider_main}
+      collapsedWidth={0}
+      width="300px"
+      theme="light"
+      // width={width > 600 ? "300px" : "200px"}
+    >
+      <div
+        className={!collapsed ? styles.logo : styles.logo_collapsed}
+        style={{
+          color: "black",
+        }}
+      >
+        Artemis
+      </div>
+      <div className={styles.divider}></div>
+      <Row className={styles.headliners}>
+        <div
+          style={{
+            fontSize: `${collapsed ? ".3rem" : "0.7rem"}`,
+            color: "black",
+            opacity: 0.5,
+          }}
+        >
+          MAIN
+        </div>
+      </Row>
+      <Menu theme="light" defaultSelectedKeys={["1"]} mode="inline">
+        <Menu.Item key="1" icon={<PieChartOutlined />}>
+          <Link to="/dashboard"> Dashboard </Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<DesktopOutlined />}>
+          <Link to="/discover">Discover</Link>
+        </Menu.Item>
+        <SubMenu key="sub1" icon={<UserOutlined />} title="Users">
+          <Menu.Item key="3">Tom</Menu.Item>
+          <Menu.Item key="4">Bill</Menu.Item>
+          <Menu.Item key="5">Alex</Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub2" icon={<TeamOutlined />} title="Documents">
+          <Menu.Item key="8">Doc1</Menu.Item>
+          <Menu.Item key="9">Doc2</Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub3" icon={<AppstoreOutlined />} title=" Applications">
+          <Menu.Item key="8">Doc1</Menu.Item>
+          <Menu.Item key="9">Doc2</Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub4" icon={<FileOutlined />} title="Pages">
+          <Menu.Item key="10">Page1</Menu.Item>
+          <Menu.Item key="11">Page2</Menu.Item>
+        </SubMenu>
+
+        <Row className={styles.headliners}>
+          <div
+            style={{
+              fontSize: `${collapsed ? ".3rem" : "0.7rem"}`,
+              color: "black",
+              opacity: 0.5,
+            }}
+          >
+            SECONDARY
+          </div>
+        </Row>
+
+        <Menu.Item key="12" icon={<QuestionCircleOutlined />}>
+          <Link to="/support_center">Support Center</Link>
+        </Menu.Item>
+        <Menu.Item key="13" icon={<InboxOutlined />}>
+          <Link to="/inbox">Inbox</Link>
+        </Menu.Item>
+        <Menu.Item key="14" icon={<FolderOpenOutlined />}>
+          <Link to="/file_manager">File manager</Link>
+        </Menu.Item>
+        <Menu.Item key="15" icon={<BarsOutlined />}>
+          <Link to="/data_list">Data List</Link>
+        </Menu.Item>
+        <Menu.Item key="16" icon={<SettingOutlined />}>
+          <Link to="/settings">Settings</Link>
+        </Menu.Item>
+        <Menu.Item key="17" icon={<LogoutOutlined />}>
+          <Link to="/log_out">Log Out</Link>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+};
+
+export default SideBar;
